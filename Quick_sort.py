440942fb7070c4c62140e9e2d2af404770fc7383
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-30 00:59:48 -0300

    Se agrego comentarios al codigo

--- Quick_sort.py
+++ Quick_sort.py
@@ -1,18 +1,25 @@
+# Funcion quick_Sort que ordena la lista pasando un array como parametro
 def quick_sort(arr):
+    # Si los elementos de la lista son menor o igual 1 entonces devuelve el array y no realiza ninguna operacion
     if len(arr)<=1:
         return arr
-    
+
     izquierda=[]
     derecha=[]
     pivote=arr[0]
 
+    # Recorre la lista y decimos que empieze de la posicion 1
     for i in range(1, len(arr)):
+    #Si es menor a pivote entonces se guarda en el array izquierda
         if pivote>arr[i]:
             izquierda.append(arr[i])
+    #Si es mayor a pivote entonces se guarda en el array derecho
         elif pivote<arr[i]:
             derecha.append(arr[i])
+    # Concatena izquierda, pivote y derecha y lo devuelve en un array 
     return quick_sort(izquierda) + [pivote] + quick_sort(derecha)
 
+# Comprobaciones de las lista
 print(quick_sort([]))
 print(quick_sort([10]))
 print(quick_sort([2,4,1,10]))
